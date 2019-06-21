57fe1ac7e0390c21271c1eb2f2b407870d681379
Jordan Pittier <jordan.pittier@streamroot.io>
2019-06-21 12:25:14 +0200

    Optionnally install Tiller manually

--- apps/helm.tf
+++ apps/helm.tf
@@ -20,7 +20,104 @@ resource "kubernetes_cluster_role_binding" "tiller" {
   subject {
     kind      = "ServiceAccount"
     name      = kubernetes_service_account.tiller.metadata[0].name
-    namespace = "kube-system"
+    namespace = kubernetes_service_account.tiller.metadata[0].namespace
   }
 }
 
+resource "kubernetes_deployment" "tiller_with_rbac" {
+  count = 0
+  metadata {
+    name      = "tiller-deploy"
+    namespace = kubernetes_service_account.tiller.metadata[0].namespace
+
+    labels = {
+      app  = "helm"
+      name = "tiller"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app  = "helm"
+        name = "tiller"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app  = "helm"
+          name = "tiller"
+        }
+      }
+
+      spec {
+        service_account_name = kubernetes_service_account.tiller.metadata[0].name
+
+        container {
+          name  = "tiller"
+          image = "gcr.io/kubernetes-helm/tiller:v2.14.0"
+
+          port {
+            name           = "tiller"
+            container_port = 44134
+          }
+
+          port {
+            name           = "http"
+            container_port = 44135
+          }
+
+          env {
+            name  = "TILLER_NAMESPACE"
+            value = kubernetes_service_account.tiller.metadata[0].namespace
+          }
+
+          env {
+            name  = "TILLER_HISTORY_MAX"
+            value = "0"
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/liveness"
+              port = 44135
+            }
+
+            initial_delay_seconds = 1
+            timeout_seconds       = 1
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/readiness"
+              port = 44135
+            }
+
+            initial_delay_seconds = 1
+            timeout_seconds       = 1
+          }
+
+          # See https://github.com/terraform-providers/terraform-provider-kubernetes/issues/38#issuecomment-318581203
+          volume_mount {
+            mount_path = "/var/run/secrets/kubernetes.io/serviceaccount"
+            name       = kubernetes_service_account.tiller.default_secret_name
+            read_only  = true
+          }
+        }
+
+        # See https://github.com/terraform-providers/terraform-provider-kubernetes/issues/38#issuecomment-318581203
+        volume {
+          name = kubernetes_service_account.tiller.default_secret_name
+
+          secret {
+            secret_name = kubernetes_service_account.tiller.default_secret_name
+          }
+        }
+      }
+    }
+  }
+}
