b0f24caab411bee347310d1fee7b4107ed2e3617
Jordan Pittier <jordan.pittier@gmail.com>
2019-07-12 08:55:49 -0700

    Prometheus RBAC: don't hardcode values in ClusterRoleBinding

--- apps/prometheus/rbac/cluster_role_binding.tf
+++ apps/prometheus/rbac/cluster_role_binding.tf
@@ -6,12 +6,12 @@ resource "kubernetes_cluster_role_binding" "prometheus" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = "prometheus"
+    name      = kubernetes_cluster_role.prometheus.metadata.0.name
   }
 
   subject {
     kind      = "ServiceAccount"
-    name      = "prometheus"
+    name      = kubernetes_service_account.prometheus.metadata.0.name
     namespace = var.namespace
   }
 }
